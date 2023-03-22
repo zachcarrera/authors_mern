a83fe82e807f8ba4786c33ee0d7a6f98f616f8d3
Zach Carrera <zachcarrera@gmail.com>
2023-03-22 07:43:32 -0700

    add Routes and Route components to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { AuthorList } from "./views/AuthorList";
+import { NewAuthor } from "./views/NewAuthor";
+import { EditAuthor } from "./views/EditAuthor";
+import { NotFound } from "./views/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Favorite Authors</h1>
+            <Routes>
+                <Route path="/" element={<AuthorList />} />
+                <Route path="/new" element={<NewAuthor />} />
+                <Route path="/edit/:_id" element={<EditAuthor />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
